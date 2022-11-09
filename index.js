48693770dd83a550f5bb4a2a76a43af9639ae91c
vale <valeryevnam.h@gmail.com>
2022-11-09 12:14:09 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,60 @@
+import validator from './validator.js';
+
+const btnAbrirModal =
+    document.querySelector("#btn-abrir-modal");
+const modal =
+    document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click", () => {
+    modal.showModal();
+
+    let verifica = document.getElementById('btnValidate');
+
+
+    verifica.addEventListener("click", () => {
+
+        let newNum = document.getElementById('cardNumber').value;
+        //  console.log(newNum, '<<< new num')
+             // alert('Completar los datos')
+        
+          if (validator.isValid(newNum) === true) {
+
+            alert('Su tarjeta es Valida :)')
+                       
+        } else {
+
+            alert('Su tarjeta no es Valida :(')
+        }
+    })
+
+    //  const newNum = document.getElementById("cardNumber");
+    // oculta.addEventListener("click", () => {         
+
+    //  validator.maskify(newNum.value);
+    // console.log(oculta);
+
+    // })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
